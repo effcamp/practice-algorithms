aa1b5f2b90f22c2e5d7b1dbc5ed6fffe211697b8
effcamp <fcf.camposx@gmail.com>
2018-01-22 14:20:46 -0200

    Momoized the recursive fibonacci function

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -7,16 +7,31 @@
 // forms the first ten entries of the fibonacci series.
 // Example:
 //   fib(4) === 3
+const now = require('performance-now');
+
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    return result;
+  };
+}
 
 function fib(n) {
   // recursive solution
+  // Awful.. n^2 run time - memoize/cache calls for better performance
 
   if (n < 2) {
     return n;
   }
   return fib(n - 1) + fib(n - 2);
 
-  //more concise - iterative
+  // more concise - iterative -- linear run time
   // const result = [0, 1];
   // for (let i = 1; i < n; i++) {
   //   const a = result[i];
@@ -41,5 +56,9 @@ function fib(n) {
   // return a;
 }
 
-console.log(fib(4));
+fib = memoize(fib);
+const t0 = now();
+console.log(fib(1000));
+const t1 = now();
+console.log(`Fib took ${t1 - t0}ms to run `);
 module.exports = fib;
